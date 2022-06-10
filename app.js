c54686642903e4229f8bc642d103fcc9f75e933e
Quentin Viergever <9016605@student.tcrmbo.nl>
2022-06-10 14:05:47 +0200

    1st game stage

---
+++ app.js
@@ -0,0 +1,65 @@
+const computerChoice = document.getElementById("computer-choice");
+const playerChoice = document.getElementById("player-choice");
+const resultOutput = document.getElementById("result");
+
+const possibleChoices = document.querySelectorAll('button');
+
+let computer;
+let player;
+let result;
+
+possibleChoices.forEach(button => button,addEventListener('click', (e) => {
+    player = e.target.id;
+    playerChoice.innerHTML = player;
+    generateComputerChoice();
+    getResult();
+}));
+
+const generateComputerChoice = () =>{
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber == 1) {
+        computer= "Rock";
+    }
+
+    if (randomNumber == 2) {
+        computer= "Paper";
+    }
+
+    if (randomNumber == 3) {
+       computer= "Scissors";
+    }
+    computerChoice.innerHTML = computer;
+}
+const getResult = () => {
+    if (computer == player){
+        result = "Gelijkspel"
+    };
+    
+    if (computer == "Rock" && player == "Paper"){
+        result = "Je hebt gewonnen!"
+    };
+        
+    if (computer == "Rock" && player == "Scissors"){
+        result = "Je hebt verloren"
+    };
+        
+    if (computer == "Paper" && player == "Scissors"){
+        result = "Je hebt gewonnen!"
+    };
+        
+    if (computer == "Paper" && player == "Rock"){
+        result = "Je hebt verloren"
+    };
+    
+    if (computer == "Scissors" && player == "Rock"){
+        result = "Je hebt gewonnen!"
+    };
+        
+    if (computer == "Scissors" && player == "Paper"){
+        result = "Je hebt verloren"
+    };
+    resultOutput.innerHTML = result;
+}
+
+
